af4c4b72d5513cc229bf1ea905634afbd463cdfa
naveed-niazi <naveedabdullah700@gmail.com>
2023-12-31 03:07:15 +0500

    Headline update

--- components/AdditionalTechnologies.tsx
+++ components/AdditionalTechnologies.tsx
@@ -5,7 +5,7 @@ import DetailTech from "./DetailTech";
 function AdditionalTechnologies() {
   return (
     <div className='mx-2 sm:mx-8 xl:mx-auto mt-24'>
-      <span className='text-3xl font-semibold'>Expertise and Skills Detail</span>
+      <span className='text-3xl font-semibold'>Expertise and Skills</span>
       <div className='grid grid-cols-1 xl:grid-cols-2 '>
         {DETAIL_TECH_LIST.map((tech, index) => (
           <DetailTech {...tech} key={index} />
